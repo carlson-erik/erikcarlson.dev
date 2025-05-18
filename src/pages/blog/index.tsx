68761d2f4feb0624bfa69511bdfcda04ed03d932
Erik Carlson <erik@erikcarlson.dev>
2025-05-17 22:06:19 -0400

    Adds reusable page metadata util and uses throughout pages. Rename PageLayout to Layout. Rename 'posts' to 'articles'.

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -3,28 +3,27 @@ import Head from "next/head";
 /* ------------------ Components ------------------ */
 import Layout from "@/components/layout";
 /* ------------------ Utils ------------------ */
-import { getSortedPostsData } from "../../../lib/posts";
+import { getSortedArticles } from "../../lib/articles";
+import { getPageMetadata } from "@/lib/metadata";
 
 export async function getStaticProps() {
-  const allPosts = getSortedPostsData();
+  const allArticles = getSortedArticles();
   return {
     props: {
-      allPosts,
+      allArticles,
     },
   };
 }
 
-export default function BlogPage({ allPosts }: { allPosts: any }) {
+export default function BlogPage({ allArticles }: { allArticles: any }) {
   return (
     <>
-      <Head>
-        <title>Blog | Erik Carlson</title>
-      </Head>
+      <Head>{getPageMetadata("Blog")}</Head>
       <ThemeProvider>
         <Layout>
           <h2>Blog</h2>
-          {allPosts.map((post: any) => {
-            return <div key={post.id}>{post.title}</div>;
+          {allArticles.map((article: any) => {
+            return <div key={article.id}>{article.title}</div>;
           })}
         </Layout>
       </ThemeProvider>
